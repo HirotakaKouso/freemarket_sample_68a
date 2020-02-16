5b986b4b9b97b295a8af45aec865cd504decb99f
adonson <adonson@users.noreply.github.com>
2020-02-16 15:33:47 +0900

    create prefectures
    create devise route
    create migration file for prefecture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  
+  def after_sign_in_path_for(resource)
+    "/"
+  end
 
   private
 
@@ -12,5 +17,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  # 入力フォームからアカウント名情報をDBに保存するために追加
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
