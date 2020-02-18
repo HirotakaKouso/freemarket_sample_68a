498f4b9fad0be6d7afec55d1951083d1d71b41b0
shinji <sshin8916@gmail.com>
2020-02-18 13:03:51 +0900

    商品確認ページの修正(scssの追加)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'   
   } 
-
+  resources :orders, only: [:new]
   devise_scope :user do
     get "user/:id", :to => "users/registrations#detail"
     get "signup", :to => "users/registrations#new"
