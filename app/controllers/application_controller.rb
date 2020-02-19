5cfafa756c3f5fc57ee9e1f7ea1c19e2fc70fae0
keita0930 <59858201+keita0930@users.noreply.github.com>
2020-02-19 11:00:25 +0900

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, except: [:index, :show, :search, :new]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   def after_sign_in_path_for(resource)
     "/"
   end
