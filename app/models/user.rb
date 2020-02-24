9af5fc03c4108968782a908fb580e8adccaae94a
adonson <adonson@users.noreply.github.com>
2020-02-24 18:59:57 +0900

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,22 +7,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
-  # validates :nickname, :only_integer, presence: true
-  # validates :password, presence: true
-  # validates :last_name, presence: true
-  # validates :first_name, presence: true
-  # validates :last_name_kana, presence: true
-  # validates :first_name_kana, presence: true
-  # validates :tel, :only_integer, presence: true
-  # validates :zip_code, :only_integer, presence: true
-  # validates :prefecture_id, :only_integer, presence: true
-  # validates :address, presence: true
-  # validates :birth_year, :only_integer, presence: true
-  # validates :birth_month, :only_integer, presence: true
-  # validates :birth_day, :only_integer, presence: true
+  validates :nickname,  presence: true
+  validates :password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :tel, presence: true
+  validates :zip_code, presence: true, numericality: { only_integer: true }
+  validates :prefecture_id, presence: true, numericality: { only_integer: true }
+  validates :address, presence: true
+  validates :birth_year, presence: true, numericality: { only_integer: true }
+  validates :birth_month, presence: true, numericality: { only_integer: true }
+  validates :birth_day, presence: true, numericality: { only_integer: true }
 
 
 
