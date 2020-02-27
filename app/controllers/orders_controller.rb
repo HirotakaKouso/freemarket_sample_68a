696f112707bf63e8c2a4d502c850c7b5b82c96db
HirotakaKouso <kousoda7@yahoo.co.jp>
2020-02-27 14:38:55 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   def new
     @order = Order.new
-    @item = Item.find_by(id: params[:format])
+    @item = Item.find(params[:format])
     @card = Card.new
   end
 end
