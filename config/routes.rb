b70bae742d9c000063059aed5683a54bb98a3870
ONITAKO1204 <59825110+ONITAKO1204@users.noreply.github.com>
2020-02-27 16:43:50 +0900

    credit_function_change

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
 
-  get 'purchase/index'
-  get 'purchase/done'
   resources :prefectures
 
   root to: 'top#index'
 
   resources :items, only: [:new, :create, :show] do
     collection do
-      get "get_category_children", defaults: { format: 'json' }
-      get "get_category_grandchildren", defaults: { format: 'json' }
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
     end
   end
+
 
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
@@ -35,7 +34,7 @@ Rails.application.routes.draw do
 
   resources :purchase, only: [:index] do
     collection do
-      post 'index', to: 'purchase#index'
+      get 'index', to: 'purchase#index'
       post 'pay', to: 'purchase#pay'
       get 'done', to: 'purchase#done'
     end
