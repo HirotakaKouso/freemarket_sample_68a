5d581314b2562c91869e991d42d10c22b6600e73
HirotakaKouso <kousoda7@yahoo.co.jp>
2020-03-02 17:10:21 +0900

    編集

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
 
   resources :card, only: [:new, :show, :destroy] do
     collection do
-      post 'create', to: 'card#create'
+      post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
     end
   end
