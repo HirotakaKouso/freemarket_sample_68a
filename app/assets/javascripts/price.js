477551472b986f51a9219620b153a801a018b078
shinji <sshin8916@gmail.com>
2020-03-05 14:40:10 +0900

    不要なファイル削除&JS追加

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -2,6 +2,11 @@ $(document).on('turbolinks:load', function(){
   $(".content__wrapper__box__left__price__main__input").change(function(){
     var price =  $(".content__wrapper__box__left__price__main__input").val();
     $(".content__wrapper__box__left__price__sub__left__span").text(price/10);
-    $(".content__wrapper__box__left__price__sub__right__span").text(price - price/10);
+    $(".content__wrapper__box__left__price__sub__right__span").text(price - price/10);//手数料計算
+
+    if (price <= 299 || price >= 50001){     //価格が指定外の場合のalert
+      alert('300~50000円で指定してください');
+      $(".content__wrapper__box__left__price__main__input").val("");
+    }
   })
 });
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,6 @@
 class Category < ApplicationRecord
   has_many :items
   has_ancestry
-  has_many :brand_categories
-  has_many :brands, through: :brand_categories 
 
   validates :name, presence:true
 end
